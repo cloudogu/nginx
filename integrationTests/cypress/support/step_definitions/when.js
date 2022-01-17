4f7a0fd6575f1c0ca69cf32adc96911aa024478e
Vincent <vincent.poneleit@cloudogu.com>
2022-01-17 14:26:11 +0100

    #51 add e2e test
    - checks for link of custom HTML page
    in warp menu

--- integrationTests/cypress/support/step_definitions/when.js
+++ integrationTests/cypress/support/step_definitions/when.js
@@ -2,6 +2,10 @@ const {
     When
 } = require("cypress-cucumber-preprocessor/steps");
 
+When(/^the user opens the alwasy existing cas ui$/, function () {
+    cy.visit(Cypress.config().baseUrl + Cypress.env('casPath'));
+});
+
 When(/^the user requests the static custom HTML page$/, function () {
-    cy.request(Cypress.env('CustomHTMLPath'));
+    cy.request(Cypress.env('customHTMLPath'));
 });
